e5d6579315e7f994906c6d532a10980fbbf94999
karamuka <shemap300@gmail.com>
2020-01-30 00:40:31 +0200

    move all test mocks to test file

--- src/app.spec.js
+++ src/app.spec.js
@@ -9,16 +9,27 @@ dotenv.config();
 
 use(chaiHttp);
 
-const {
-  TEST_EMAIL, TEST_PASSWORD, TEST_ANNOUNCEMENT_ID,
-} = process.env;
-
-const TEST_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1IjoiZGE1MTA1YzI3YjQ4MTk2ZjJhOWU2ZGE3MTRmZmIzODEzYzYxZmE4MDUwMGJjZTI4ZmUwOWQ2OTZhNWYyNDNmMmJmNWMzNzRhMTNmMGYxMWNhOThiYWVhYWRiODQ4MGRhIiwiaWF0IjoxNTgwMzIzMTIyLCJleHAiOjE1ODA5Mjc5MjJ9._rY2_iUdroE2ZNPz7pB265qIMZkf3BFEWUeoV6HlqkA';
-
-const testCredentials = {
-  email: TEST_EMAIL,
-  password: TEST_PASSWORD,
-  token: TEST_TOKEN,
+const testData = {
+  credentials: {
+    email: 'test@announceit.com',
+    password: 'test@announceit',
+  },
+  announcement: {
+    id: 1580329945061,
+    title: 'my custom test announcement title',
+    text: 'my custom test announcement text',
+    startDate: '2019-12-31T22:00:00',
+    endDate: '2020-02-29T22:00:00',
+  },
+  newUser: {
+    email: `${Date.now()}test@announceit.com`,
+    password: 'test@announceit',
+    firstName: 'john',
+    lastName: 'doe',
+    phoneNumber: 250722111111,
+    address: 'Test address',
+  },
+  token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1IjoiZGE1MTA1YzI3YjQ4MTk2ZjJhOWU2ZGE3MTRmZmIzODEzYzYxZmE4MDUwMGJjZTI4ZmUwOWQ2OTZhNWYyNDNmMmJmNWMzNzRhMTNmMGYxMWNhOThiYWVhYWRiODQ4MGRhIiwiaWF0IjoxNTgwMzIzMTIyLCJleHAiOjE1ODA5Mjc5MjJ9._rY2_iUdroE2ZNPz7pB265qIMZkf3BFEWUeoV6HlqkA',
 };
 
 describe('User', () => {
@@ -26,18 +37,10 @@ describe('User', () => {
     it('should create a new user account', (done) => {
       request(app)
         .post('/api/v1/auth/signup')
-        .send({
-          email: `${Date.now()}test@announceit.com`,
-          password: 'test@announceit',
-          firstName: 'john',
-          lastName: 'doe',
-          phoneNumber: 250722111111,
-          address: 'Test address',
-        })
+        .send(testData.newUser)
         .end((err, res) => {
           expect(res.status).to.equal(201);
           if (err) {
-            console.log(err);
             return done(err);
           }
           return done();
@@ -46,18 +49,10 @@ describe('User', () => {
     it('should not create a user with invalid input', (done) => {
       request(app)
         .post('/api/v1/auth/signup')
-        .send({
-          email: `${Date.now()}test@announceit`,
-          password: 'test',
-          firstName: 'john',
-          lastName: 'doe',
-          phoneNumber: 2507221,
-          address: 'Test address',
-        })
+        .send({ phoneNumber: 2507221 })
         .end((err, res) => {
           expect(res.status).to.equal(422);
           if (err) {
-            console.log(err);
             return done(err);
           }
           return done();
@@ -66,7 +61,7 @@ describe('User', () => {
     it('should authenticate a user with valid credentials', (done) => {
       request(app)
         .post('/api/v1/auth/signin')
-        .send(testCredentials)
+        .send(testData.credentials)
         .end((err, res) => {
           expect(res.status).to.equal(201);
           if (err) {
@@ -96,13 +91,8 @@ describe('User', () => {
     it('should create a new announcement', (done) => {
       request(app)
         .post('/api/v1/announcement')
-        .send({
-          title: 'my custom test announcement title',
-          text: 'my custom test announcement text',
-          startDate: '2019-01-01',
-          endDate: '2020-01-01',
-        })
-        .set('Authorization', TEST_TOKEN)
+        .send(testData.announcement)
+        .set('Authorization', testData.token)
         .end((err, res) => {
           expect(res.status).to.equal(201);
           if (err) {
@@ -120,7 +110,7 @@ describe('User', () => {
           startDate: '2019-01-01-01',
           endDate: '2020-01-01',
         })
-        .set('Authorization', TEST_TOKEN)
+        .set('Authorization', testData.token)
         .end((err, res) => {
           expect(res.status).to.equal(422);
           if (err) {
@@ -131,8 +121,8 @@ describe('User', () => {
     }).timeout(15000);
     it('should update a specific announcement', (done) => {
       request(app)
-        .patch(`/api/v1/announcement/${+TEST_ANNOUNCEMENT_ID}`)
-        .set('Authorization', TEST_TOKEN)
+        .patch(`/api/v1/announcement/${testData.announcement.id}`)
+        .set('Authorization', testData.token)
         .send({
           text: 'new updated text from test',
         })
@@ -146,8 +136,8 @@ describe('User', () => {
     }).timeout(15000);
     it('advertiser should not update announcement status', (done) => {
       request(app)
-        .patch(`/api/v1/announcement/${+TEST_ANNOUNCEMENT_ID}`)
-        .set('Authorization', TEST_TOKEN)
+        .patch(`/api/v1/announcement/${testData.announcement.id}`)
+        .set('Authorization', testData.token)
         .send({
           status: 'active',
         })
@@ -161,7 +151,7 @@ describe('User', () => {
     }).timeout(15000);
     it('should not update an announcement with invalid auth token', (done) => {
       request(app)
-        .patch(`/api/v1/announcement/${+TEST_ANNOUNCEMENT_ID}`)
+        .patch(`/api/v1/announcement/${testData.announcement.id}`)
         .set('Authorization', 'vvbifbiusfvoun')
         .send({
           status: 'active',
@@ -177,7 +167,7 @@ describe('User', () => {
     it('should view all his/her announcements', (done) => {
       request(app)
         .get('/api/v1/announcement')
-        .set('Authorization', TEST_TOKEN)
+        .set('Authorization', testData.token)
         .end((err, res) => {
           expect(res.status).to.equal(200);
           if (err) {
@@ -201,7 +191,7 @@ describe('User', () => {
     it('should view announcements by status', (done) => {
       request(app)
         .get('/api/v1/announcement/?status=pending')
-        .set('Authorization', TEST_TOKEN)
+        .set('Authorization', testData.token)
         .end((err, res) => {
           expect(res.status).to.equal(200);
           if (err) {
@@ -212,8 +202,8 @@ describe('User', () => {
     }).timeout(15000);
     it('should view a specific announcement', (done) => {
       request(app)
-        .get(`/api/v1/announcement/${+TEST_ANNOUNCEMENT_ID}`)
-        .set('Authorization', TEST_TOKEN)
+        .get(`/api/v1/announcement/${testData.announcement.id}`)
+        .set('Authorization', testData.token)
         .end((err, res) => {
           expect(res.status).to.equal(200);
           if (err) {
@@ -225,7 +215,7 @@ describe('User', () => {
     it('should not view an announcement with invalid id', (done) => {
       request(app)
         .get('/api/v1/announcement/1545')
-        .set('Authorization', TEST_TOKEN)
+        .set('Authorization', testData.token)
         .end((err, res) => {
           expect(res.status).to.equal(404);
           if (err) {
@@ -236,7 +226,7 @@ describe('User', () => {
     }).timeout(15000);
     it('should not view an announcement with invalid auth token', (done) => {
       request(app)
-        .get(`/api/v1/announcement/${TEST_ANNOUNCEMENT_ID}`)
+        .get(`/api/v1/announcement/${testData.announcement.id}`)
         .set('Authorization', 'INVALID=ll_0bisvonlsfkvsvlmsfvh')
         .end((err, res) => {
           expect(res.status).to.equal(401);
